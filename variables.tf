ace3d771403047c4b69f5155164824fdec05f48b
Irek Romaniuk <irekromaniuk@Ireks-Mac-mini.local>
2020-03-10 14:27:07 -0400

    security rule added

--- variables.tf
+++ variables.tf
@@ -1,5 +1,4 @@
 variable "create_address_object" {
-  type        = "integer"
   default     = 0
 }
 
@@ -29,7 +28,6 @@ variable "address_object_tag" {
 }
  
 variable "create_security_rule" {
-  type        = "integer"
   default     = 0
 }
 
